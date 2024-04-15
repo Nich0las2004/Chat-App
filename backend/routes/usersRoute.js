d956601113e72f071a8128d8f9555772901e5343
Nika Oniani <nikaoniani2018@gmail.com>
2024-04-16 00:26:08 +0400

    Updated the .gitignore file

--- backend/routes/usersRoute.js
+++ backend/routes/usersRoute.js
@@ -12,7 +12,7 @@ import authenticateToken from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
 router.post("/token", (req, res) => {
-  const refreshToken = req.cookies.accessToken;
+  const refreshToken = req.cookies.refreshtoken;
   if (refreshToken == null) {
     return res.sendStatus(401);
   }
