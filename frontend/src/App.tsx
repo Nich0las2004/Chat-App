757eea4d7caf0a7449083b7100daf19e624bcda9
Nika Oniani <nikaoniani2018@gmail.com>
2024-04-20 16:39:28 +0400

    frontend communicates with the backend using axios

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes, Navigate } from "react-router-dom";
 
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import RoomPage from "./pages/RoomPage";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
+      <Route path="/room" element={<RoomPage />} />
     </Routes>
   );
 };
--- frontend/src/components/SignIn/SignIn.tsx
+++ frontend/src/components/SignIn/SignIn.tsx
@@ -5,10 +5,10 @@ import { SignInHeader } from "./SignInHeader";
 const SignIn = () => {
   return (
     <div className="relative flex min-h-screen  antialiased flex-col justify-center overflow-hidden bg-gray-900 py-6 sm:py-12">
-      <form className="bg-gray-900  relative py-3 sm:w-96 mx-auto text-center">
+      <div className="bg-gray-900  relative py-3 sm:w-96 mx-auto text-center">
         <SignInHeader />
         <SignInForm />
-      </form>
+      </div>
     </div>
   );
 };
