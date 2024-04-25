bba360411646449bd65c7977fc5ae73022272a36
Nika Oniani <nikaoniani2018@gmail.com>
2024-04-25 16:40:33 +0400

    added authSlice, which makes us know if a user is logged in or not

---
+++ frontend/src/features/authSlice.tsx
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isAuthenticated: false,
+  refreshToken: null,
+  accessToken: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    login(state, action) {
+      state.isAuthenticated = true;
+      state.refreshToken = action.payload.refreshToken;
+      state.accessToken = action.payload.accessToken;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+      state.refreshToken = null;
+      state.accessToken = null;
+    },
+  },
+});
+
+export {login,logout} from authSlice
