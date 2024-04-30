36291f1204f830e12cbe175cb27e3c789f999304
Nika Oniani <nikaoniani2018@gmail.com>
2024-04-30 23:05:44 +0400

    a user can send a message to a room, his username will be displayed just above the message content when he sends a message

--- frontend/src/components/Message/Message.tsx
+++ frontend/src/components/Message/Message.tsx
@@ -1,9 +1,16 @@
-const Message = ({ message }) => {
+import { useSelector } from "react-redux";
+
+const Message = ({ message }: { message: string }) => {
+  const username = useSelector((state) => state.user.username);
+
   return (
     <div className="flex mb-2">
       <div className="bg-gray-200 rounded-lg p-2">
-        <div className="font-semibold"></div>
-        <div>{message}</div>
+        <div className="flex items-center mb-1">
+          <div className="w-8 h-8 bg-gray-400 rounded-full flex-shrink-0 mr-2"></div>
+          <div className="font-semibold">{username}</div>
+        </div>
+        <div className="text-sm text-gray-700">{message}</div>
       </div>
     </div>
   );
