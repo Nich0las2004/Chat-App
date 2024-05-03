af10a27a7c9f6fc32e7244e1c20636c2f446a3e5
Nika Oniani <nikaoniani2018@gmail.com>
2024-05-04 00:51:23 +0400

    the chat looks dark themed, improved button and input styling, as well as message template, now it goes well with the website

--- frontend/src/components/Room/MainRoom.tsx
+++ frontend/src/components/Room/MainRoom.tsx
@@ -7,7 +7,7 @@ import { useDispatch, useSelector } from "react-redux";
 const MainRoom = () => {
   const dispatch = useDispatch();
   const [input, setInput] = useState<string>("");
-  // const [roomNumber, setRoomNumber] = useState<number>(1);
+  const [roomNumber, setRoomNumber] = useState<number>(1);
 
   const messagesArr = useSelector((state) => state.message.messages);
 
@@ -19,6 +19,14 @@ const MainRoom = () => {
 
   const messageHandler = (e) => {
     setInput(e.target.value);
+  };
+
+  const joinRoomHandler = () => {
+    socket.emit("join-room", roomNumber);
+  };
+
+  const handleInputChange = (e) => {
+    setRoomNumber(parseInt(e.target.value, 10));
   };
 
   useEffect(() => {
@@ -38,30 +46,40 @@ const MainRoom = () => {
 
   return (
     <div className="flex flex-col h-screen">
-      <div className="flex-grow bg-gray-200 p-4 overflow-y-auto">
+      <div className="flex-grow bg-gray-900 p-4 overflow-y-auto">
         {messagesArr.map((m: string) => (
           <Message message={m.message} />
         ))}
       </div>
-      <div className="bg-gray-300 p-4 flex items-center">
+      <div className="bg-gray-800 p-4 flex items-center">
         {/* Message input */}
         <input
           type="text"
           onChange={messageHandler}
-          className="flex-grow bg-white border border-gray-400 rounded px-4 py-2 mr-2 focus:outline-none focus:border-blue-500"
+          className="flex-grow bg-gray-700 text-white border border-gray-600 rounded px-4 py-2 mr-2 focus:outline-none focus:border-purple-500"
           placeholder="Type your message..."
         />
         {/* Send button */}
         <button
           onClick={sendHandler}
-          className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+          className="bg-purple-600 hover:bg-purple-700 focus:outline-none px-4 py-2 rounded-md text-white"
         >
           Send
         </button>
       </div>
-      <div className="bg-gray-300 p-4 flex items-center">
-        <input type="number" />
-        <button className="bg-green-500 text-white px-4 py-2 rounded-md ml-2 hover:bg-green-600 focus:outline-none focus:bg-green-600">
+
+      <div className="bg-gray-800 p-4 flex items-center">
+        <input
+          type="number"
+          onChange={handleInputChange}
+          value={roomNumber}
+          className="bg-gray-700 text-white px-3 py-2 rounded-md focus:outline-none mr-2"
+          placeholder="Enter room number"
+        />
+        <button
+          onClick={joinRoomHandler}
+          className="bg-purple-600 hover:bg-purple-700 focus:outline-none px-4 py-2 rounded-md text-white"
+        >
           Join Room
         </button>
       </div>
