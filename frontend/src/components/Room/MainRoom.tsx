32a1e187da445386349f1e6e62b92154f1d51821
Nika Oniani <nikaoniani2018@gmail.com>
2024-05-18 23:08:00 +0400

    added loader/button, when a user is joining a custom room, he/she will get a message saying 'Joining Room'

--- frontend/src/components/Room/MainRoom.tsx
+++ frontend/src/components/Room/MainRoom.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, Fragment } from "react";
 import { useNavigate } from "react-router-dom";
 import { socket } from "../../services/socket";
 import Message from "../Message/Message";
@@ -10,11 +10,13 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 import { resetUser } from "../../features/userSlice";
 import { logout } from "../../features/authSlice";
+import Loading from "../Loading/Loading";
 
 const MainRoom = () => {
   const dispatch = useDispatch();
   const [input, setInput] = useState<string>("");
   const [roomNumber, setRoomNumber] = useState<number>(1);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const messagesArr = useSelector((state) => state.message.messages);
 
@@ -32,6 +34,7 @@ const MainRoom = () => {
   };
 
   const joinRoomHandler = () => {
+    setIsLoading(true);
     socket.emit("create-room", roomNumber, (message: string) =>
       console.log(message)
     );
@@ -41,7 +44,10 @@ const MainRoom = () => {
 
     axios.get(`http://localhost:5555/room/${roomNumber}`).then((res) => {
       console.log(res);
-      navigate(`/room/${roomNumber}`);
+      setTimeout(() => {
+        navigate(`/room/${roomNumber}`);
+        setIsLoading(false);
+      }, 2000);
     });
   };
 
@@ -80,62 +86,67 @@ const MainRoom = () => {
   }, []);
 
   return (
-    <div className="flex flex-col h-screen">
-      <div className="bg-gray-900 p-4 flex items-center justify-between">
-        <div className="text-purple-300 mr-4">User: test</div>
-        <div className="text-purple-300 mr-4">Room</div>
-        <Link to="/login">
-          <button
-            title="Log Out"
-            className="bg-red-600 hover:bg-red-700 focus:outline-none px-4 py-2 rounded-md text-white"
-            onClick={logoutHandler}
-          >
-            {<FontAwesomeIcon icon={faRightFromBracket} />}
-            {/* Logout */}
-          </button>
-        </Link>
-      </div>
-      <div className="flex-grow bg-gray-900 p-4 overflow-y-auto">
-        {messagesArr.map((m: string) => (
-          <Message message={m.message} />
-        ))}
-      </div>
-      <div className="bg-gray-800 p-4 flex items-center">
-        {/* Message input */}
-        <input
-          type="text"
-          onChange={messageHandler}
-          value={input}
-          className="flex-grow bg-gray-700 text-white border border-gray-600 rounded px-4 py-2 mr-2 focus:outline-none focus:border-purple-500"
-          placeholder="Type your message..."
-        />
-        {/* Send button */}
-        <button
-          onClick={sendHandler}
-          className="bg-purple-600 hover:bg-purple-700 focus:outline-none px-4 py-2 rounded-md text-white"
-        >
-          Send
-        </button>
-      </div>
+    <Fragment>
+      {isLoading && <Loading />}
+      {!isLoading && (
+        <div className="flex flex-col h-screen">
+          <div className="bg-gray-900 p-4 flex items-center justify-between">
+            <div className="text-purple-300 mr-4">User: test</div>
+            <div className="text-purple-300 mr-4">Room</div>
+            <Link to="/login">
+              <button
+                title="Log Out"
+                className="bg-red-600 hover:bg-red-700 focus:outline-none px-4 py-2 rounded-md text-white"
+                onClick={logoutHandler}
+              >
+                {<FontAwesomeIcon icon={faRightFromBracket} />}
+                {/* Logout */}
+              </button>
+            </Link>
+          </div>
+          <div className="flex-grow bg-gray-900 p-4 overflow-y-auto">
+            {messagesArr.map((m: string) => (
+              <Message message={m.message} />
+            ))}
+          </div>
+          <div className="bg-gray-800 p-4 flex items-center">
+            {/* Message input */}
+            <input
+              type="text"
+              onChange={messageHandler}
+              value={input}
+              className="flex-grow bg-gray-700 text-white border border-gray-600 rounded px-4 py-2 mr-2 focus:outline-none focus:border-purple-500"
+              placeholder="Type your message..."
+            />
+            {/* Send button */}
+            <button
+              onClick={sendHandler}
+              className="bg-purple-600 hover:bg-purple-700 focus:outline-none px-4 py-2 rounded-md text-white"
+            >
+              Send
+            </button>
+          </div>
 
-      <div className="bg-gray-800 p-4 flex items-center">
-        <input
-          type="number"
-          onChange={handleInputChange}
-          value={roomNumber}
-          min={2}
-          pattern="[1-9][0-9]*"
-          className="bg-gray-700 text-white px-3 py-2 rounded-md focus:outline-none mr-2"
-          placeholder="Enter room number"
-        />
-        <button
-          onClick={joinRoomHandler}
-          className="bg-purple-600 hover:bg-purple-700 focus:outline-none px-4 py-2 rounded-md text-white"
-        >
-          Join Room
-        </button>
-      </div>
-    </div>
+          <div className="bg-gray-800 p-4 flex items-center">
+            <input
+              type="number"
+              onChange={handleInputChange}
+              value={roomNumber}
+              min={2}
+              pattern="[1-9][0-9]*"
+              className="bg-gray-700 text-white px-3 py-2 rounded-md focus:outline-none mr-2"
+              placeholder="Enter room number"
+            />
+            <button
+              onClick={joinRoomHandler}
+              className="bg-purple-600 hover:bg-purple-700 focus:outline-none px-4 py-2 rounded-md text-white"
+            >
+              Join Room
+            </button>
+          </div>
+        </div>
+      )}
+    </Fragment>
   );
 };
 export default MainRoom;
