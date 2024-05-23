2c09a68ddbeab2198be29f8a639755a187f28fcf
Nika Oniani <nikaoniani2018@gmail.com>
2024-05-23 21:54:48 +0400

    added type to events

--- frontend/src/components/Room/MainRoom.tsx
+++ frontend/src/components/Room/MainRoom.tsx
@@ -29,7 +29,7 @@ const MainRoom = () => {
     }
   };
 
-  const messageHandler = (e) => {
+  const messageHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInput(e.target.value);
   };
 
@@ -51,7 +51,7 @@ const MainRoom = () => {
     });
   };
 
-  const handleInputChange = (e) => {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setRoomNumber(parseInt(e.target.value, 10));
   };
 
